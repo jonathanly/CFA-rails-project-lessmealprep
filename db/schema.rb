18c10f09cfddc22408f1adb34dd1c1f1033bde81
Jonathan Ly <jonathanlyhv@gmail.com>
2016-11-22 14:29:36 +1100

    Created Product model w/ relationship to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122001903) do
+ActiveRecord::Schema.define(version: 20161122012826) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "price"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "username"
